abb2a4741883fb0ece4885c85f6b0e65c6f4efcd
Alexej Krzewitzki <alexej.k@ultrabold.com>
2023-08-11 17:50:14 +0200

    Updated publish command with force option

--- src/CleverReach/Commands/PublishCommand.php
+++ src/CleverReach/Commands/PublishCommand.php
@@ -6,18 +6,18 @@ use Illuminate\Console\Command;
 
 class PublishCommand extends Command
 {
-    protected $signature = 'clever-reach:publish';
+    protected $signature = 'clever-reach:publish {--force : Overwrite all previously published assets}';
 
     protected $description = 'Publish CleverReach boiler plate files';
 
     public function handle()
     {
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-config']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-migrations']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-models']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-routes']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-actions']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-livewire']);
-        $this->call('vendor:publish', ['--tag' => 'clever-reach-views']);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-config', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-migrations', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-models', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-routes', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-actions', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-livewire', '--force' => $this->option('force')]);
+        $this->call('vendor:publish', ['--tag' => 'clever-reach-views', '--force' => $this->option('force')]);
     }
 }
